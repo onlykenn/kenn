7f2ac4a131e98ab70156344ca7f27e7fe0ba539e
onlykenn <iofi.ranz@gmail.com>
2022-08-08 22:04:00 +0700

    apalo

---
+++ plugins/info-donasi.js
@@ -0,0 +1,17 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn }) => {
+let pp = await conn.profilePictureUrl(m.chat).catch(_ => null)
+let pepe = pp ? await (await fetch(pp)).buffer() : Buffer.alloc(0)
+let str = ` ${conn.getName(m.sender)} Want Support Bot?
+Dana-Gopay-Pulsa
+6283897011555`
+conn.sendHydrated(m.chat, str, wm, pepe, 'https://wa.me/6283897011555', 'ChatMe', null, null, [
+['𝙼𝙴𝙽𝚄', '/menu']
+], m)
+
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+export default handler
---
+++ plugins/weebs-loli.js
@@ -0,0 +1,19 @@
+import Booru from 'booru'
+import fetch from 'node-fetch'
+let sites = ['sb', 'kn', 'kc']
+
+let handler = async (m, { conn, usedPrefix, command }) => {
+	let res = await Booru.search(sites.getRandom(), ['loli'], { random: true })
+	let url = res[0].fileUrl
+	conn.sendButton(m.chat, 'Random Image Loli', await shortUrl(url), url, [['Next', usedPrefix + command]], m)
+}
+handler.help = ['l1']
+handler.tags = ['owner']
+handler.command = /^(l)$/i
+handler.owner = true
+
+export default handler
+
+async function shortUrl(url) {
+	return await (await fetch(`https://tinyurl.com/api-create.php?url=${url}`)).text()
+}
